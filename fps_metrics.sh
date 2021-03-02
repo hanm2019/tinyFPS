2b53e18f2f64c11b9c40f7b0dfd2cb094c83f01a
hanmeng <hanm@buaa.edu.cn>
2021-03-02 16:22:56 +0800

    push all hls code

--- fps_metrics.sh
+++
@@ -1,2 +0,0 @@
-python script/fps_metrics.py --dataset modelnet40 --num_sampling 16,32,64 --batch_size 8 --metrics cover,distance,minradiu --solution 13 
-python script/fps_metrics.py --dataset KITTI --num_sampling 512,1024,2048  --batch_size 1 --metrics cover,distance,minradiu --solution 13  
